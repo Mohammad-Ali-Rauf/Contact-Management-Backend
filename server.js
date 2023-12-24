6cd50322a9e33f8fb7d7dfbdf7201a23d3122b83
Mohammad-Ali-Rauf <m.aliadnanrauf@gmail.com>
2023-12-24 10:10:17 +0500

    -

--- server.js
+++ server.js
@@ -11,16 +11,13 @@ connectDB();
 app.use(express.json({ extended: false }))
 
 // Define Routes here
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
+app.use('/api/users', require('./api/users'));
+app.use('/api/auth', require('./api/auth'));
+app.use('/api/contacts', require('./api/contacts'));
 
-// Serve static files on production server
-if(process.env.NODE_ENV === 'production') {
-    // Set Static Folder
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
-}
+app.get('/', (req, res) => {
+    res.json({ msg: 'Welcome to the Contact Manager API' })
+})
 
 const PORT = process.env.PORT || 5000;
 
